556d1382dceda984fb90ab458a7959369c982f2d
Yi-Hsun Chou <Yi-HsunChou@Yi-Hsuns-MacBook-Air.local>
2016-11-12 16:03:44 +0800

    - Add Dashboard

--- routes/views.js
+++ routes/views.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('views', { title: '查看今日訂閱狀況 | Order Lunch' });
 });
 
 module.exports = router;
